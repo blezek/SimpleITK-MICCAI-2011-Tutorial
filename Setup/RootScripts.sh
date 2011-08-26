a7de8bfd6a7da9032fb63f211270879c0c7186c4
Daniel Blezek <blezek.daniel@mayo.edu>
2011-08-26 14:03:43 -0500

    Updated root script to install OpenCV

--- Setup/RootScripts.sh
+++ Setup/RootScripts.sh
@@ -1,7 +1,7 @@
 # Package management
 sudo apt-get remove cmake
 sudo apt-get install libncurses5-dev ipython
-sudo apt-get install libcv2.1 libcv-dev python-dev emacs vim tcl-dev tk-dev ruby-dev
+sudo apt-get install python-dev emacs vim tcl-dev tk-dev ruby-dev
 sudo apt-get make gcc git g++
 
 # install Java, accept the license
@@ -22,3 +22,9 @@ sudo apt-get install ipython-qtconsole
 
 cd /tmp/ && wget http://www.cmake.org/files/v2.8/cmake-2.8.5.tar.gz && tar fxvz cmake-2.8.5.tar.gz
 cd cmake-2.8.5/ && sudo ./configure --prefix=/usr && sudo make install
+
+# OpenCV
+sudo add-apt-repository ppa:gijzelaar/opencv2
+sudo apt-get update
+sudo apt-get install opencv
+sudo apt-get install libopencv-dev
